94e2c107288aed319d6c654485522feddbd7df56
Liz <pansyway8@gmail.com>
2014-12-03 16:23:06 -0600

    Make meetup a class, add all groups and test table with multiple events

--- womenTechSite/techCalendar/meetup.py
+++ womenTechSite/techCalendar/meetup.py
@@ -12,20 +12,22 @@ import models as tcm
 # >>> url = 'https://api.twitter.com/1.1/account/verify_credentials.json'
 # >>> auth = OAuth1('YOUR_APP_KEY', 'YOUR_APP_SECRET',
 #                   'USER_OAUTH_TOKEN', 'USER_OAUTH_TOKEN_SECRET')
+class MeetupEvent:
+
+    def __init__( self ):
+        self.meetupApiHost = "http://api.meetup.com"
+        self.eventGet = "/2/events"
+        #self.meetupGroups = [ 'Girl-Develop-It-Minneapolis']
+
+        # TODO; Make this configurable
+        self.meetupGroups = [ 'Girl-Develop-It-Minneapolis', 'PyLadiesTC', \
+        'Girls-in-Tech-Minneapolis', 'Twin-Cities-Visualization-Group']
 
 
-meetupApiHost = "http://api.meetup.com"
-eventGet = "/2/events"
-meetupGroups = [ 'Girl-Develop-It-Minneapolis']
-
-#meetupGroups = [ 'Girl-Develop-It-Minneapolis', 'PyLadiesTC', \
-#        'Girls-in-Tech-Minneapolis', 'Twin-Cities-Visualization-Group']
-meetupQuery = ['group_urlname', 'omit']
-omitField = [ 'venue', 'fee']
-
-
-def makeMeetupPayload ( meetupGroup ): 
-    return { 'group_urlname':meetupGroup, 'omit':'venue,fee', 'key':ak.meetupKey, 'fields':'timezone' }
+    def makeMeetupPayload ( self, meetupGroupName ): 
+        return { 'group_urlname':meetupGroupName, 
+                'omit':'venue,fee', 'key':ak.meetupKey, 
+                'fields':'timezone' }
 
 
 # eventBriteHost = "http://www.eventbriteapi.com"
@@ -36,33 +38,37 @@ def makeMeetupPayload ( meetupGroup ):
 # These groups don't have a lot of events to warrent a page limit
 # Sample Url 
 # https://api.meetup.com/2/events?group_urlname=Girl-Develop-It-Minneapolis&omit=venue,fee&key=<meetup key here>
+    def storeEvents ( self ):
+        meetupUrl = self.meetupApiHost + self.eventGet
 
-meetupUrl = meetupApiHost + eventGet
+        # grab json from each meetup group
+        # API only allows for one group to be requested at once
+        for m in self.meetupGroups:
+            payload = self.makeMeetupPayload(m)
+            r = requests.get(meetupUrl, params=payload)
 
-# grab json from each meetup group
-# API only allows for one group to be requested at once
-for m in meetupGroups:
-    payload = makeMeetupPayload(m)
-    r = requests.get(meetupUrl, params=payload)
+            meetupEvents = r.json()['results']
+            for ev in meetupEvents:
+                print ev['event_url']
+                print ev['group']['name']
+                print ev['time'] # milliseconds since the epoch
+                print ev['timezone']
+                print ev['duration']
+                print ev['name']
 
-    meetupEvents = r.json()['results']
-    for ev in meetupEvents:
-        print ev['event_url']
-        print ev['group']['name']
-        print ev['time'] # milliseconds since the epoch
-        print ev['timezone']
-        print ev['duration']
-        print ev['name']
+                startTimeUTC = ev['time']/1000
+                endTimeUTC = (ev['time'] + ev['duration'])/1000
+                (tg, created) = tcm.TechGroup.objects.get_or_create( name = ev['group']['name'] )
+            
+                (ce, created) = tcm.CalendarEvent.objects.get_or_create( group = tg,
+                                                                     title = ev['name'],
+                                                                     start_datetime = dt.datetime.fromtimestamp(startTimeUTC),
+                                                                     end_datetime = dt.datetime.fromtimestamp(endTimeUTC) ,
+                                                                     link = ev['event_url'])
 
-        startTimeUTC = ev['time']/1000
-        endTimeUTC = (ev['time'] + ev['duration'])/1000
-        (tg, created) = tcm.TechGroup.objects.get_or_create( name = ev['group']['name'] )
-    
-        (ce, created) = tcm.CalendarEvent.objects.get_or_create( group = tg,
-                                                             title = ev['name'],
-                                                             start_datetime = dt.datetime.fromtimestamp(startTimeUTC),
-                                                             end_datetime = dt.datetime.fromtimestamp(endTimeUTC) ,
-                                                             link = ev['event_url'])
 
+#if __name__ == __main__:
+mue = MeetupEvent()
+mue.storeEvents()
 
 
--- womenTechSite/techCalendar/views.py
+++ womenTechSite/techCalendar/views.py
@@ -7,38 +7,10 @@ from techCalendar.models import CalendarEvent
 
 # Create your views here.
 def index(request):
-    events_list = CalendarEvent.objects.all()
+    events_list = CalendarEvent.objects.order_by( 'start_datetime' )
     template = loader.get_template('techCalendar/index.html')
     context = RequestContext ( request ,
         { 'events_list': events_list, 
         })
     return HttpResponse(template.render(context))
 
-# Taken from stackoverflow
-def eventsFeed(request):
-    from django.utils.timezone import utc
-    from django.core.serializers.json import DjangoJSONEncoder
-
-    if request.is_ajax():
-        print 'Its ajax from fullCalendar()'
-
-    try:
-        start = datetime.fromtimestamp(int(request.GET.get('start', False))).replace(tzinfo=utc)
-        end = datetime.fromtimestamp(int(request.GET.get('end',False)))
-    except ValueError:
-        start = datetime.now.replace(tzinfo=utc)
-        end = start + timedelta(days=7)
-
-    entries = CalendarEvents.objects.filter(date__gte=start).filter(date__lte=end)
-    print entries
-    json_list = []
-    for entry in entries:
-        id = entry.id
-        title = entry.title
-        start = entry.date.strftime("%Y-%m-%dT%H:%M:%S")
-        allDay = False
-
-        json_entry = {'id':id, 'start':start, 'allDay':allDay, 'title': title}
-        json_list.append(json_entry)
-
-    return HttpResponse(json.dumps(json_list), content_type='application/json')
